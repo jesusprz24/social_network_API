d136484b3ecdbdf8c2c50a9f925c741bcb0bcabb
Jesus <perez.jesus1039@gmail.com>
2023-04-01 21:10:11 -0700

    commit before my brain melts

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,34 @@
+const {Schema, model, Types} = require('mongoose');
+const moment = require('moment');
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.OkectId,
+            default: () => new Types.ObjectId,
+        },
+        rectionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+            transform: (doc, ret) => {
+                ret.createdAt = moment(rat.createdAt).format('mm-dd-yyyy, h:mm:ss A');
+                return ret;
+            },
+        },
+        id: false,
+    }
+);
