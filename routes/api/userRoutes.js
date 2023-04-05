f216cec1a0aee426b61352d9349047eafe16af63
Jesus <perez.jesus1039@gmail.com>
2023-04-04 19:43:25 -0700

    created the userRoutes for the get, post, put, delete to be used in the userController

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../../controllers/userController');
+
+router.get('/users', userController.getUsers);
+router.get('/users/:userId', userController.getSingleUser);
+router.post('/users', userController.createUser);
+router.put('/users/:userId', userController.updateUser);
+router.delete('/users/:userId', userController.deleteUser);
+router.post('/users/:userId/friends', userController.addFriend);
+router.delete('/users/:userId/friends/:friendId', userController.deleteFriend);
+
+module.exports = router;
 
