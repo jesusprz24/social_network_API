282f41ca6e294bcfb0eb30e86ea3e025a5ab5561
Jesus <perez.jesus1039@gmail.com>
2023-04-04 18:47:22 -0700

    created the user schema to create a new student model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,32 @@
+const {Schema, model} = require('mongoose');
+const Thought = require('./Thought');
 
+// schema used for the student model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/,'Please enter a valid email' ]
+        },
+
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'thought'
+        }],  
+    }, 
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
